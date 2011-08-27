437d9c4e6c53a5ee25cad47df9a1257e7f93e0c5
Scott Lawrence <yorgle@gmail.com>
2011-08-27 17:01:50 -0400

    Adding the sample sounds. These are Creative Commons licensed.  Feel free to reuse them in your projects. I created them in the mid 90s using Cool Edit, for a game that was not completed.

---
+++ SoundEngineTest/SampleAudio/Sounds/convertit.sh
@@ -0,0 +1,26 @@
+afconvert  -d I8 ./lightning/bounce.wav caf/lightning_bounce.caf
+afconvert  -d I8 ./lightning/crash.wav caf/lightning_crash.caf
+afconvert  -d I8 ./lightning/explode.wav caf/lightning_explode.caf
+afconvert  -d I8 ./lightning/fire.wav caf/lightning_fire.caf
+afconvert  -d I8 ./lightning/idle.wav caf/lightning_idle.caf
+afconvert  -d I8 ./lightning/lofire.wav caf/lightning_lofire.caf
+afconvert  -d I8 ./lightning/thrust.wav caf/lightning_thrust.caf
+afconvert  -d I8 ./misc/bonkecho.wav caf/misc_bonkecho.caf
+afconvert  -d I8 ./misc/explode\ backup.wav caf/misc_explode_back.caf
+afconvert  -d I8 ./misc/explode_rumble.wav caf/misc_explode_rumble.caf
+afconvert  -d I8 ./misc/noise1.wav caf/misc_noise1.caf
+afconvert  -d I8 ./misc/noise2.wav caf/misc_noise2.caf
+afconvert  -d I8 ./misc/siren1.wav caf/misc_siren.caf
+afconvert  -d I8 ./trad/bounce.wav caf/trad_bounce.caf
+afconvert  -d I8 ./trad/crash.wav caf/trad_crash.caf
+afconvert  -d I8 ./trad/explode.wav caf/trad_explode.caf
+afconvert  -d I8 ./trad/fire.wav caf/trad_fire.caf
+afconvert  -d I8 ./trad/idle.wav caf/trad_idle.caf
+afconvert  -d I8 ./trad/thrust.wav caf/trad_thrust.caf
+afconvert  -d I8 ./ui/accept.wav caf/ui_accept.caf
+afconvert  -d I8 ./ui/accept2.wav caf/ui_accept2.caf
+afconvert  -d I8 ./ui/CLICK_L.WAV caf/ui_click_l.caf
+afconvert  -d I8 ./ui/CLICK_S.WAV caf/ui_click_s.caf
+afconvert  -d I8 ./ui/denied.wav caf/ui_denied.caf
+afconvert  -d I8 ./ui/exclmtn.wav caf/ui_exclmtn.caf
+afconvert  -d I8 ./ui/question.wav caf/ui_question.caf
